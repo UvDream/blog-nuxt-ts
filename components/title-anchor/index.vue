6b2975ee1ea689504263e8332d8401121fe70998
wangzhongjie <uvdream@163.com>
2020-06-19 15:40:40 +0800

    fix🐛 修复目录

--- components/title-anchor/index.vue
+++ components/title-anchor/index.vue
@@ -2,13 +2,13 @@
  * @Author: wangzhongjie
  * @Date: 2020-04-16 16:43:04
  * @LastEditors: wangzhongjie
- * @LastEditTime: 2020-04-30 15:47:19
+ * @LastEditTime: 2020-06-19 15:39:33
  * @Description: 文章目录
  * @Email: UvDream@163.com
  -->
 <template>
   <Affix :offset-top="50">
-    <Anchor show-ink class="anchor">
+    <!-- <Anchor show-ink class="anchor">
       <AnchorLink
         v-for="item in data"
         :key="item.id"
@@ -21,9 +21,20 @@
           :key="j.id"
           :href="'#' + j.title"
           :title="j.title"
+
         ></AnchorLink>
       </AnchorLink>
-    </Anchor>
+    </Anchor> -->
+    <outline-tree :treeData="data" class="anchor" ref="tree">
+    <div slot-scope="{ data, parentData, level }">
+      <div
+        class="node-render-content"
+        :class="{active:data.id==activeId}"
+        @click.stop="jumpToAnchor(data.id)">
+        {{ data.title }}
+      </div>
+    </div>
+  </outline-tree>
   </Affix>
 </template>
 
@@ -37,18 +48,32 @@ export default {
   data() {
     return {
       titleArr: [],
-      spinShow: true
+      spinShow: true,
+      activeId:''
     };
   },
   computed: {},
   created() {},
   mounted() {
     this.titleArr = Array.from(this.data);
+    this.activeId =  this.titleArr[0]['id']
   },
   watch: {
    
   },
-  methods: {},
+  methods: {
+    jumpToAnchor (id) {
+      //字体变色
+      this.activeId = id
+      let element = document.getElementById(id)
+      if (element) {
+          window.scrollTo({
+                    top:element.offsetTop,
+                    behavior:'smooth'
+          })
+      }
+    }
+  },
   components: {}
 };
 </script>
@@ -64,5 +89,9 @@ export default {
   border: 1px solid var(--borderColor);
   font-size: 16px;
   margin-top: 1rem;
+  cursor: default;
+}
+.active {
+  color: #2d8cf0!important;
 }
 </style>
